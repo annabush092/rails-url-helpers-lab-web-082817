374e8003b93cae7e1551111357301d7f8f2e1bbe
annabush092 <annabush092@gmail.com>
2017-09-22 17:31:44 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,28 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
-    @students = Student.all
+    @all_students = Student.all
+
+    #implicit render 'students/index'
   end
 
   def show
+    set_student
+    if @student.active
+      @active = "active"
+    else
+      @active = "inactive"
+    end
+    #implicit render 'students/show'
+  end
+
+  def update
+    #the put/patch method to update @student
+    set_student
+    switch = !(@student.active)
+    @student.update(active: switch)
+    redirect_to student_path(@student)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+  #equivalent of
+  # get "students", to: "students#index"
   resources :students, only: :index
+
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get "students/:id", to: "students#show"
+  get "students/:id", to: "students#show", as: 'student'
+
+  get "students/:id/activate", to: "students#update", as: 'activate_student'
 end
